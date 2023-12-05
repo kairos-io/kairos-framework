851f08ba1265436c9ff43274eeefe5d042801f4e
Itxaka <itxaka@kairos.io>
2023-12-05 18:52:13 +0100

    Make script have a default file but accept overrides
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- .github/yaml2md.sh
+++ .github/yaml2md.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-
+if [ "$1" == "" ];then
+  FILE=versions.yaml
+else
+  FILE=$1
+fi
 echo "| Name | Category | Version |"
 echo "|------|----------|---------|"
 
-yq e '.[] | [.name, .category, .version] | @tsv' $1 | while IFS=$'\t' read -r name category version
+yq e '.[] | [.name, .category, .version] | @tsv' "${FILE}" | while IFS=$'\t' read -r name category version
 do
   echo "| $name | $category | $version |"
 done
